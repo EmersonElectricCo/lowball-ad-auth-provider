c7fa86b04d143c2fcb710c21a55feb49ad4c504b
Timothy Lemm <compsecmonkey@gmail.com>
2021-05-07 21:12:19 -0500

    #1 base structure of the provider

---
+++ lowball-ldap/auth_provider.py
@@ -0,0 +1,125 @@
+from lowball.models.provider_models.auth_provider import AuthProvider, AuthPackage
+from lowball.models.authentication_models import ClientData
+from lowball.exceptions import MalformedAuthPackageException, InvalidCredentialsException
+
+
+class LDAPAuthProvider(AuthProvider):
+    """Default Auth Provider for Lowball Applications
+    This is the primary class for the lowball-ldap Authentication Provider.
+    :param username: the username of service account able to lookup and validate users.
+    :type username: str
+    :param password: the password of the service account.
+    :type password: str
+    """
+    def __init__(self, service_account, service_account_password, hostname, base_dn,  ):
+        super(LDAPAuthProvider, self).__init__()
+
+        if not isinstance(username, str):
+            raise TypeError("username must be a string")
+        self._username = username
+
+        if not isinstance(password, str):
+            raise TypeError("password must be a string")
+        self._password = password
+
+        #service_account, r
+        #service_account_password,r
+        #protocol, o, ldap
+        #base_dn, r
+        #hostname, r
+        #protocol_version, o,3
+        #domain, r
+        #roll_mappings, o, {}
+        #username_ldap_attribute, r
+        #port, o, defaults to the particular ldap protocol
+
+        # protocol to connect to the LDAP instance (ldap or ldaps)
+        protocol = "ldap"
+
+        # This is the attribute that it is going to attempt to match off of
+        # for searching for a user
+        username_ldap_attribute = 'sAMAccountName'
+
+        # Base DN to search through: ex: 'DC=<my org>, DC=org'
+        base_dn = ""
+
+        # Protocol to use
+        protocol_version = 3
+
+        # hostname for ldap server
+        hostname = ""
+
+        domain = 'EMRSN\\'
+
+        roll_mappings = {}
+
+    @property
+    def username(self):
+        """Get the username needed for authentication"""
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        """Can't set username after init"""
+        raise PermissionError("cannot set username after init")
+
+    @property
+    def password(self):
+        """Get the password needed for authentication"""
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        """Can't set password after init"""
+        raise PermissionError("cannot set password after init")
+
+    def authenticate(self, auth_package):
+        """Authenticate a user.
+        something something
+        :param auth_package: data needed to authenticate with this provider
+        :type auth_package: DefaultAuthPackage
+        :return: auth data
+        :rtype: AuthData
+        """
+        pass
+
+    def get_client(self, client_id):
+        pass
+
+    @property
+    def auth_package_class(self):
+        """The auth package class that this class' `authenticate` method accepts."""
+        return LDAPAuthPackage
+
+
+class LDAPAuthPackage(AuthPackage):
+    def __init__(self, username, password, **kwargs):
+        super(LDAPAuthPackage, self).__init__(**kwargs)
+        self.username = username
+        self.password = password
+
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self, value):
+        if not isinstance(value, str):
+            raise TypeError("username must be a string")
+        self._username = value
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self, value):
+        if not isinstance(value, str):
+            raise TypeError("password must be a string")
+        self._password = value
+
+
+__all__ = [
+    "LDAPAuthProvider",
+    "LDAPAuthPackage"
+    ]
